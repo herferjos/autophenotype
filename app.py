a0d5391e1aefebaed0a40eccff2b19376c004808
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-20 12:16:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 
-
 if 'index_database' not in st.session_state:
     # reconstruir_faiss()
     st.session_state['index_database'] = faiss.read_index("index.faiss")
@@ -22,3 +21,21 @@ if 'chatbot' not in st.session_state:
     sign = Login(st.secrets['email'], st.secrets['password'])
     cookies = sign.login()
     st.session_state['chatbot'] = hugchat.ChatBot(cookies=cookies.get_dict())
+
+st.markdown("<h2 style='text-align: center;'>¡Buscador de Fenotipos Humanos!</h2>", unsafe_allow_html=True)
+st.write("### 1) Introduce los síntomas que deseas buscar")
+descripcion = st.text_area(label=":blue[Descripción clínica]", placeholder="Escribe aquí...")
+
+if st.button(label = "Extraer Síntomas", type = "primary"):
+    st.session_state['description'] = descripcion
+    with st.spinner("Estamos procesando tu petición, puede tardar unos minutos..."):
+        st.session_state['df_sintomas'] = orchest(descripcion)
+
+st.write("---")
+if 'description' in st.session_state:
+    st.write("#### Síntomas:")
+    st.write(st.session_state.description)
+    
+if 'df_sintomas' in st.session_state:
+    st.write('### 2) Fenotipos encontrados')
+    st.data_editor(st.session_state.df_sintomas, use_container_width=True, num_rows="dynamic", disabled=False, hide_index = True)
